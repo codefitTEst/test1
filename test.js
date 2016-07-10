9192d24bb6c0ea0e32f495fe8a19585ba9e0c5ff
codefitTEst <codefit@fernandotrujano.com>
2016-07-10 08:55:25 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,3 @@
 //Testing javascript
 
 console.log("Hello world"); 
-
-a = [1,2,3,4,5,6,7]
-
-a.map(function(a){return a+2}); 
-
-
-for (i in a){ 
-  console.log(i)
-}
-
-//TODO Fix my bugs and stuff
-//TODO Also shower!!!!
-//TODO rescans work!
-// class a = 5. 
-
-// module.exports = test
